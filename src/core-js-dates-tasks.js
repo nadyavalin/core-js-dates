f2c09765ad8d17aa974076604262bf98d1c516e9
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-03-04 19:45:12 +0300

    feat: make the getWeekNumberByDate function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -203,8 +203,10 @@ function getCountWeekendsInMonth(month, year) {
  * Date(2024, 0, 31) => 5
  * Date(2024, 1, 23) => 8
  */
-function getWeekNumberByDate(/* date */) {
-  throw new Error('Not implemented');
+function getWeekNumberByDate(date) {
+  const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+  const pastDaysOfYear = (date - firstDayOfYear) / (24 * 60 * 60 * 1000);
+  return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
 }
 
 /**
