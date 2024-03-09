2f4c297a45f151252bfce3fbac2b8c3522fb64f2
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2024-03-09 09:27:27 +0300

    feat: make the getWorkSchedule function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -275,8 +275,27 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  throw new Error('Not implemented');
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const start = new Date(period.start.split('-').reverse().join('-'));
+  const end = new Date(period.end.split('-').reverse().join('-'));
+  let current = new Date(start);
+  const workDatesArray = [];
+
+  while (current <= end) {
+    for (let i = 0; i < countWorkDays; i += 1) {
+      if (current <= end) {
+        const workDay = current.toLocaleDateString('ru-Ru', {
+          year: 'numeric',
+          month: 'numeric',
+          day: 'numeric',
+        });
+        workDatesArray.push(workDay.replaceAll('.', '-'));
+      }
+      current = new Date(current.setDate(current.getDate() + 1));
+    }
+    current = new Date(current.setDate(current.getDate() + countOffDays));
+  }
+  return workDatesArray;
 }
 
 /**
